c88f0c667df7b736b4afefa60830db9af94ab3f8
YuriProc <urupa.y@toplyvo.app>
2020-02-18 16:11:56 +0200

    Fix xPath in CreateNewCompanyV2

--- app/tests_modules/company_create_new_v2.js
+++ app/tests_modules/company_create_new_v2.js
@@ -95,7 +95,8 @@ let CompanyCreateNewV2 = async (browser, page, CompanyData) => {
         //Опять Ждём Загрузки Страницы (на всякий пожарный)
         await WaitUntilPageLoads(page);
         // Проверим Наличие ИНПУТА (Тип компании)
-        xPath = '//input[@name="company_types"]';
+        //xPath = '//input[@name="company_types"]';
+        xPath = `//label[@class="select__label"][contains(text(), "Тип компании")]`;
         resOk = await WaitForElementIsPresentByXPath(12000, page, xPath);
         if (!resOk) {
             throw `FAIL => Не вижу (${xPath})`;//<--специальный вызов ошибки!
